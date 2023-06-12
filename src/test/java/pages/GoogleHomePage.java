2631b702a745d96f012937c9739c537c0e19a64a
Zulfiia <burkhanovaz@gmail.com>
2023-06-12 15:00:52 -0400

    Adding new pages

---
+++ src/test/java/pages/GoogleHomePage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleHomePage {
+
+    public GoogleHomePage () {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
